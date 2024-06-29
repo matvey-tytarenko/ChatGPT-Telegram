ef4dbdc9a9a796d375d9cd3ab63904db567f7c2e
“Matvey <matveytytarenko25@gmail.com>
2024-06-29 19:19:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import telebot
+from telebot import types
+import config
+
+from g4f.client import Client
+
+bot = telebot.TeleBot(config.API)
+
+def gpt(prompt):
+    client = Client()
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{
+        "role": "user",
+        "content": prompt,
+    }],
+    )
+    return response.choices[0].message.content
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    send = f"Привет {message.from_user.first_name} я бесплатный ChatGPT4 можешь задавать мне любые вопросы!"
+    bot.send_message(message.chat.id, text=send)
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot.send_message(message.chat.id, gpt(message.text))
+
+bot.polling(none_stop=True)
